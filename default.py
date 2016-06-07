55e0feef3ded229ade68d82ea24863308c042515
Jason Borowski <esc0rtd3w@gmail.com>
2016-06-07 09:05:44 -0400

    Updated Page Dump Stuff [TESTING]
    WILL PRODUCE LONG LOAD TIMES BECAUSE OF SCRAPE TESTING

--- default.py
+++ default.py
@@ -39,6 +39,8 @@ PLUGIN_NAME = "plugin.video.cinemassacre"
 
 site_base = "http://cinemassacre.com/"
 
+site_xml = 'site.xml'
+
 try:
     import StorageServer
 except:
@@ -47,7 +49,7 @@ except:
 # cache for one hour
 cache = StorageServer.StorageServer(PLUGIN_NAME, 1)
 
-cache.table_name = PLUGIN_NAME
+#cache.table_name = PLUGIN_NAME
 #cache.set("some_string", "string")
 #save_data = { "some_string": "string", "some_int": 1234, "some_dict": repr({ "our": { "dictionary": [] } }) }
 #cache.setMulti("pre-", save_data)
@@ -65,6 +67,21 @@ def videoIdYoutube(value):
         return youtube_regex_match.group(6)
 
     return youtube_regex_match
+	
+	
+def findValidLinks(value):
+    
+    # Get "Dated" URLs
+    get_dated = (r'(\/\d\d\d\d\/\d\d\/\d\d\/)')
+    #get_dated = (r'(\/\d\d\d\d\/\d\d\/\d\d\/)[^\d]')
+    temp_link = re.match(get_dated, value)
+    logPlus(temp_link, "temp_link: ")
+	
+    if temp_link:
+        return temp_link.group(6)
+
+    return temp_link
+	
 
 def getSignature(key, msg):
     return base64.b64encode(hmac.new(key, msg, hashlib.sha1).digest())
@@ -83,29 +100,16 @@ def logPlus(msg, label):
 
 # Read Content From XML
 def getContentFromXML():
-
-    # devicetoken=binascii.b2a_hex(os.urandom(32))
-    # deviceuid=binascii.b2a_hex(os.urandom(20)).upper()
-    # signature=getSignature(os.urandom(20),os.urandom(20))
-    # timestamp=datetime.now().strftime("%Y-%m-%dT%H:%M:%S.000Z")
-    #URL="http://cinemassacre.screenwavemedia.com/AppServer/SWMAppFeed.php?appname=Cinemassacre&appversion=1.5.8&devicetoken="+devicetoken+"&deviceuid="+deviceuid+"&lastupdateid=0&timestamp="+timestamp+"&signature="+signature
-    #req = urllib2.Request(URL)
-    #response = urllib2.urlopen(req)
-    #xml = response.read()
-    #response.close()
 	
     addon = xbmcaddon.Addon()
     addon_path = addon.getAddonInfo('path')
-    _path = os.path.join(addon_path,'site.xml')
+    _path = os.path.join(addon_path, site_xml)
     f = open(_path, 'r')
     xml = f.read()
     return xmltodict.parse(xml)['document']
 
 
-def getCategoriesFromXML(content,id):
-
-    #logPlus(content, "content: ")
-    #logPlus(id, "id: ")
+def getCategoriesFromXML(content, id):
 	
     items = []
 	
@@ -189,6 +193,9 @@ def pageDump(web_url, page_num):
         #logPlus(html, "html: ")
         #logPlus(temp_grab, "temp_grab: ")
 		
+        #findValidLinks(temp_grab)
+
+		
     # Get "link" for XML Output
     for temp_grab in soup.find_all('a'):
         temp_link = (temp_grab.get('href'))
@@ -201,6 +208,7 @@ def pageDump(web_url, page_num):
 	
 	
 def findSections(source, text):
+
     soup = BeautifulSoup(source, "html.parser")
     sections = soup.find_all("div", {"class": "archiveitem"})
     for sec in sections:
@@ -210,13 +218,149 @@ def findSections(source, text):
         yield url, name, img,size
 
 
-		
-pageDump(site_base + "category/avgn/avgnepisodes", "1")
-pageDump(site_base + "category/avgn/avgnepisodes", "2")
-pageDump(site_base + "category/avgn/avgnepisodes", "3")
-pageDump(site_base + "category/avgn/avgnepisodes", "4")
-pageDump(site_base + "category/avgn/avgnepisodes", "5")
-pageDump(site_base + "category/avgn/avgnepisodes", "6")
+def dumpSite():
+    
+    # Angry Video Game Nerd
+    pageDump(site_base + "category/avgn/avgnepisodes", "1")
+    pageDump(site_base + "category/avgn/avgnepisodes", "2")
+    pageDump(site_base + "category/avgn/avgnepisodes", "3")
+    pageDump(site_base + "category/avgn/avgnepisodes", "4")
+    pageDump(site_base + "category/avgn/avgnepisodes", "5")
+    pageDump(site_base + "category/avgn/avgnepisodes", "6")
+	
+    # James and Mike Mondays
+    pageDump(site_base + "category/jamesandmike", "1")
+    pageDump(site_base + "category/jamesandmike", "2")
+    pageDump(site_base + "category/jamesandmike", "3")
+    pageDump(site_base + "category/jamesandmike", "4")
+    pageDump(site_base + "category/jamesandmike", "5")
+    pageDump(site_base + "category/jamesandmike", "6")
+    pageDump(site_base + "category/jamesandmike", "7")
+	
+    # Mike and Ryan
+    pageDump(site_base + "category/mikeryantalkaboutgames", "1")
+	
+    # Mike and Bootsy
+    pageDump(site_base + "category/mike-bootsy", "1")
+	
+    # Board James
+    pageDump(site_base + "category/boardjames", "1")
+    pageDump(site_base + "category/boardjames", "2")
+	
+    # You Know Whats Bullshit
+    pageDump(site_base + "category/ykwb", "1")
+    pageDump(site_base + "category/ykwb", "2")
+	
+    # Mikes Gaming Videos
+    pageDump(site_base + "category/mikevideos", "1")
+    pageDump(site_base + "category/mikevideos", "2")
+    pageDump(site_base + "category/mikevideos", "3")
+	
+    # Bootsy Beats
+    pageDump(site_base + "category/bootsy-beats", "1")
+	
+    # James Gaming Videos
+    pageDump(site_base + "category/jamesgamingvideos", "1")
+	
+    # Other Gaming Videos
+    pageDump(site_base + "category/othergaming-videos", "1")
+	
+    # Game Collection
+    pageDump(site_base + "2007/03/22/game-collection", "1")
+	
+    # Movie Reviews A-Z
+    pageDump(site_base + "category/moviereviewsatoz", "1")
+    pageDump(site_base + "category/moviereviewsatoz", "2")
+    pageDump(site_base + "category/moviereviewsatoz", "3")
+    pageDump(site_base + "category/moviereviewsatoz", "4")
+    pageDump(site_base + "category/moviereviewsatoz", "5")
+    pageDump(site_base + "category/moviereviewsatoz", "6")
+    pageDump(site_base + "category/moviereviewsatoz", "7")
+    pageDump(site_base + "category/moviereviewsatoz", "8")
+    pageDump(site_base + "category/moviereviewsatoz", "9")
+    pageDump(site_base + "category/moviereviewsatoz", "10")
+    pageDump(site_base + "category/moviereviewsatoz", "11")
+    pageDump(site_base + "category/moviereviewsatoz", "12")
+    pageDump(site_base + "category/moviereviewsatoz", "13")
+    pageDump(site_base + "category/moviereviewsatoz", "14")
+    pageDump(site_base + "category/moviereviewsatoz", "15")
+	
+    # Top Tens
+    pageDump(site_base + "category/moviereviews/top-tens", "1")
+	
+    # Animation Related
+    pageDump(site_base + "category/moviereviews/animation-moviereviews", "1")
+	
+    # Commentaries
+    pageDump(site_base + "category/moviereviews/commentaries", "1")
+	
+    # Interviews
+    pageDump(site_base + "category/moviereviews/interviews", "1")
+	
+    # Location Tours
+    pageDump(site_base + "category/location-tours", "1")
+	
+    # Monster Madness
+    pageDump(site_base + "category/moviereviews/monstermadness", "1")
+	
+    # Trivia Videos
+    pageDump(site_base + "category/trivia-videos", "1")
+	
+    # Other Movie Related Videos
+    pageDump(site_base + "category/othermovierelatedvideos", "1")
+	
+    # Original Films Main
+    pageDump(site_base + "category/films", "1")
+    pageDump(site_base + "category/films", "2")
+    pageDump(site_base + "category/films", "3")
+    pageDump(site_base + "category/films", "4")
+	
+    # Favorites
+    pageDump(site_base + "category/films/favorites", "1")
+	
+    # Animation
+    pageDump(site_base + "category/films/animation", "1")
+	
+    # Horror Films
+    pageDump(site_base + "category/films/horror-films", "1")
+    pageDump(site_base + "category/films/horror-films", "2")
+	
+    # Comedy
+    pageDump(site_base + "category/films/comedy", "1")
+	
+    # 48-Hour Films
+    pageDump(site_base + "category/films/48-hour-films", "1")
+	
+    # Other
+    pageDump(site_base + "category/films/other", "1")
+	
+    # Music Main
+    pageDump(site_base + "category/music-2", "1")
+	
+    # Audio Slaughter
+    pageDump(site_base + "category/music-2/audio-slaughter", "1")
+	
+    # Kyle Justin
+    pageDump(site_base + "category/music-2/kylejustin", "1")
+	
+    # Name That Tune
+    pageDump(site_base + "category/music-2/namethattune", "1")
+	
+    # Site Main
+    pageDump(site_base + "category/site-2", "1")
+	
+    # Articles
+    pageDump(site_base + "category/site-2/featuredarticles", "1")
+	
+    # Appearances
+    pageDump(site_base + "category/site-2/appearances", "1")
+	
+    # Misc Videos
+    pageDump(site_base + "category/site-2/misc-videos", "1")
+
+
+
+dumpSite()
 
 
 xbmcplugin.setContent(addon_handle, "episodes")
